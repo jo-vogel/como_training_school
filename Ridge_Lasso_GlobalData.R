9ccf948759edece0f2d591e95434be90bde4db72
PauRiv <pauline.rivoire@giub.unibe.ch>
2019-10-28 14:31:55 +0100

    First steps: get data and yield thresholds

--- Ridge_Lasso_GlobalData.R
+++ Ridge_Lasso_GlobalData.R
@@ -6,6 +6,11 @@
 ##############################################################################
 
 
+# which method? model_name in c("Ridge", "Lasso)
+model_name <- "Ridge"
+
+#threshold for bad yields in c(0.025,0.05,0.1)
+threshold <- 0.05
 
 #############################
 ##### Standardised data #####
@@ -32,14 +37,31 @@ path_to_NH_files <- "C:/Users/admin/Documents/Damocles_training_school_Como/Grou
 
 nh_files <- list.files(path=path_to_NH_files,pattern="*NH.nc") # all files from northern hemisphere
 nh_data <- lapply(1:length(nh_files),
-                  function(x){nc_open(paste0(path_to_NH_files,"/",nh_files[x]))})
+                  FUN = function(x){nc_open(paste0(path_to_NH_files,"/",nh_files[x]))})
 nh_variables <- lapply(1:length(nh_files),
-                       function(x){ncvar_get(nh_data[[x]])})
+                       FUN = function(x){ncvar_get(nh_data[[x]])})
+
 lati <- ncvar_get(nh_data[[1]],"lat")
 long <- ncvar_get(nh_data[[1]],"lon")
 lapply(1:length(nh_files),function(x){nc_close(nh_data[[x]])})  
 
-# ## Adjust growing season #####
-gs_start <- nh_variables[[which(nh_files=="crop_sowing_date_NH.nc")]][,,1] # Julian date of start of season
-gs_length <- matrix(data=nh_variables[[which(nh_files=="crop_growingseason_length_NH.nc")]],nrow=320*76,ncol=1600)
-max_gs_length <- apply(gs_length,1,max) # maximum growing season length
+
+#standardisation
+T1 <- Sys.time()
+yield_stand <- aperm(apply(nh_variables[[which(nh_files=="crop_yield_NH.nc")]],
+                           FUN = scale, MARGIN = c(1,2)), perm = c(2,3,1))
+T2 <- Sys.time()
+difftime(T2,T1)
+
+dim(nh_variables[[which(nh_files=="meteo_pr_NH.nc")]])
+
+
+#get yield threshold
+yield_treshold <- apply(nh_variables[[which(nh_files=="crop_growingseason_length_NH.nc")]],
+                        MARGIN = c(1,2), quantile, probs = threshold, na.rm=T)
+
+# Adjust growing season
+sowing_date <- nh_variables[[which(nh_files=="crop_sowing_date_NH.nc")]][,,1] # Julian date of start of season
+gs_length <- array(data=nh_variables[[which(nh_files=="crop_growingseason_length_NH.nc")]],
+                   dim = dim(nh_variables[[which(nh_files=="crop_growingseason_length_NH.nc")]]))
+max_gs_length <- apply(gs_length,MARGIN = c(1,2),max) # maximum growing season length
