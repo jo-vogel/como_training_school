93bd5455fbc4c9797ea7bf4f0fb3f8c833f0657c
PauRiv <pauline.rivoire@giub.unibe.ch>
2019-11-13 10:51:53 +0100

    Add ggplot

--- Ridge_Lasso_GlobalData.R
+++ Ridge_Lasso_GlobalData.R
@@ -9,7 +9,7 @@ print("Are you sure you want to run the next line? ;) everything in the environm
 rm(list=ls(all=TRUE))
 
 # which method? model_name in c("Ridge", "Lasso)
-model_name <- "Lasso"
+model_name <- "Ridge"
 stopifnot(model_name %in% c("Ridge", "Lasso"))
 
 if(model_name=="Lasso"){
@@ -33,10 +33,12 @@ segreg_th <- 0.5
 ##### Initialisation, librairies, data #####
 
 library(ncdf4);library(rgdal);library(raster);library(RColorBrewer);library(viridis)
+library(maps);library(mapdata);library(ggplot2)
 library(glmnet);library(InformationValue);library(ROCR)
 library(abind);library(stringr)
 library(foreach);library(doParallel)
 library(tictoc)
+
 
 #############################
 ##### Standardised data #####
@@ -94,15 +96,16 @@ cy_reshaped <- array(data=cy,dim=c(dim(cy)[1],1,1600))
 Model_data[,1,] <-cy_reshaped
 Model_data_stand[,1,] <-cy_reshaped
 
-columnnames <- c("Yield","pr_Aug_Y1","pr_Sep_Y1","pr_Oct_Y1","pr_Nov_Y1","pr_Dec_Y1","pr_Jan_Y2",
-                 "pr_Feb_Y2","pr_Mar_Y2","pr_Apr_Y2","pr_May_Y2","pr_Jun_Y2","pr_Jul_Y2",
-                 "pr_Aug_Y2","pr_Sep_Y2","pr_Oct_Y2","pr_Nov_Y2","pr_Dec_Y2",
+columnnames <- c("Yield",
                  "tmax_Aug_Y1","tmax_Sep_Y1","tmax_Oct_Y1","tmax_Nov_Y1","tmax_Dec_Y1","tmax_Jan_Y2",
                  "tmax_Feb_Y2","tmax_Mar_Y2","tmax_Apr_Y2","tmax_May_Y2","tmax_Jun_Y2","tmax_Jul_Y2",
                  "tmax_Aug_Y2","tmax_Sep_Y2","tmax_Oct_Y2","tmax_Nov_Y2","tmax_Dec_Y2",
                  "vpd_Aug_Y1","vpd_Sep_Y1","vpd_Oct_Y1","vpd_Nov_Y1","vpd_Dec_Y1","vpd_Jan_Y2",
                  "vpd_Feb_Y2","vpd_Mar_Y2","vpd_Apr_Y2","vpd_May_Y2","vpd_Jun_Y2","vpd_Jul_Y2",
-                 "vpd_Aug_Y2","vpd_Sep_Y2","vpd_Oct_Y2","vpd_Nov_Y2","vpd_Dec_Y2")
+                 "vpd_Aug_Y2","vpd_Sep_Y2","vpd_Oct_Y2","vpd_Nov_Y2","vpd_Dec_Y2",
+                 "pr_Aug_Y1","pr_Sep_Y1","pr_Oct_Y1","pr_Nov_Y1","pr_Dec_Y1","pr_Jan_Y2",
+                 "pr_Feb_Y2","pr_Mar_Y2","pr_Apr_Y2","pr_May_Y2","pr_Jun_Y2","pr_Jul_Y2",
+                 "pr_Aug_Y2","pr_Sep_Y2","pr_Oct_Y2","pr_Nov_Y2","pr_Dec_Y2")
 colnames(Model_data) <- columnnames
 colnames(Model_data_stand) <- columnnames
 
@@ -213,23 +216,23 @@ for (i in 1:pix_num){
 
 
 
-no_cores <- detectCores() / 2 - 1
-cl<-makeCluster(no_cores)
-clusterEvalQ(cl, {
-  library(glmnet)
-  library(dplyr)
-}) # parallelisation has own environment, therefore some packages and variables need be loaded again
-registerDoParallel(cl)
-
-tic()
-# cv_fit <- foreach (i=1:dim(Model_data)[1],.multicombine=TRUE) %dopar% {
-cv_fit <- foreach (i=1:5,.multicombine=TRUE) %dopar% {
-  cv.glmnet(x = as.matrix(x1_train_list[[i]]),
-            y = as.matrix(y1_train_list[[i]]),
-            family = "binomial", alpha = no_model, nfolds = 10)
-}
-stopCluster(cl)
-toc()
+# no_cores <- detectCores() / 2 - 1
+# cl<-makeCluster(no_cores)
+# clusterEvalQ(cl, {
+#   library(glmnet)
+#   library(dplyr)
+# }) # parallelisation has own environment, therefore some packages and variables need be loaded again
+# registerDoParallel(cl)
+# 
+# tic()
+# # cv_fit <- foreach (i=1:dim(Model_data)[1],.multicombine=TRUE) %dopar% {
+# cv_fit <- foreach (i=1:5,.multicombine=TRUE) %dopar% {
+#   cv.glmnet(x = as.matrix(x1_train_list[[i]]),
+#             y = as.matrix(y1_train_list[[i]]),
+#             family = "binomial", alpha = no_model, nfolds = 10)
+# }
+# stopCluster(cl)
+# toc()
 
 
 #without paralellizing
@@ -244,9 +247,13 @@ for (pixel in 1:dim(Model_data_stand)[1]) {
 }#end for pixel
 
 toc()
-#7310 sec for 940 pixels
+#15 min for Ridge
 save(model_cv_fitting, file = paste("C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/",
-                                    model_name,"_thresoldbadyield", str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+                                    model_name,"_Thresholdbadyield", str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+
+
+
+
 
 
 # Load the fitted model ####
@@ -254,6 +261,11 @@ save(model_cv_fitting, file = paste("C:/Users/admin/Documents/Damocles_training_
 
 load(file = paste("C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/",
                   model_name,"_thresoldbadyield", str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+
+
+
+
+
 
 
 # Model performance assessment ####
@@ -274,7 +286,7 @@ mypred <- lapply(1:test_length, function(x){predict(model_cv_fitting[[x]],
 
 fitted.results_model <- lapply(1:test_length, function(x){ifelse(mypred[[x]] > segreg_th,1,0)})
 
-mis_clas_err <- lapply(1:test_length, function(x){misClassError(actuals = as.matrix(y1_test_list[[x]]),
+mis_clas_err <- sapply(1:test_length, function(x){misClassError(actuals = as.matrix(y1_test_list[[x]]),
                                                                 predictedScores=mypred[[x]],
                                                                 threshold = segreg_th)})
 
@@ -292,6 +304,27 @@ speci <- sapply(1:test_length, function(x){InformationValue::specificity(as.matr
 
 # Plot specificity and sensitivity on a map ####
 
+# ggplot ####
+world <- map_data("world")
+DF_miscla <- data.frame(lon=coord_subset[,1], lat = coord_subset[,2], miscla = mis_clas_err)
+
+ggplot(data = DF_miscla, aes(x=lon, y=lat)) +
+  geom_polygon(data = world, aes(long, lat, group=group),
+               fill="transparent", color="black", size=0.3) +
+  geom_point(shape=15, aes(color=miscla)) +
+  scale_color_gradient(limits=c(0,(floor(max(mis_clas_err)*100) + 1)/100),
+                        low = "green", high = "red") +
+  theme_void()+
+  coord_fixed(xlim = c(min(coord_subset[,1])-1, max(coord_subset[,1]+1)),
+              ylim = c(min(coord_subset[,2])-1, max(coord_subset[,2]+1)),
+              ratio = 1.3)+
+  labs(color="Misclassification\n error",
+       title = paste(model_name,"regression"),
+       subtitle = paste("Bad yield threshold=", threshold,
+                        ", segregation threshold=", segreg_th, sep = ""))+
+  X11(width = 20, height = 10)
+
+
 # Workaround: bind lat and lon to one object, so that you have to look for just one object, and not lat-/lon-pairs
 coord_subset_temp <- cbind(coord_subset,paste(coord_subset[,1],coord_subset[,2]))
 coord_all_temp <- cbind(coord_all,paste(coord_all[,1],coord_all[,2]))
@@ -299,19 +332,25 @@ loc_pix <- which(coord_all_temp[,3] %in% coord_subset_temp [,3]) # locations of 
 
 coord_all <- cbind(coord_all,rep(NA,24320))
 coord_all_sensi <- cbind(coord_all,rep(NA,24320))
+coord_all_classerr <- cbind(coord_all,rep(NA,24320))
 for (i in 1:test_length){
   coord_all[loc_pix[i],3] <- speci[i]
   coord_all_sensi[loc_pix[i],3] <- sensi[i]
+  coord_all_classerr[loc_pix[i],3] <- mis_clas_err[[i]]
 }
 
 spec_mat <- matrix(as.numeric(coord_all[,3]),nrow=320,ncol=76)
 sensi_mat <- matrix(as.numeric(coord_all_sensi[,3]),nrow=320,ncol=76)
+miscla_mat <- matrix(as.numeric(coord_all_classerr[,3]),nrow=320,ncol=76)
 
 border <- readOGR('C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/ne_50m_admin_0_countries.shp')	
-spec_ras_speci <- raster(t(spec_mat[,76:1]), xmn=min(lon_all), xmx=max(lon_all), ymn=min(lat_all), ymx=max(lat_all), crs=CRS(projection(border)))
-sensi_ras_speci <- raster(t(sensi_mat[,76:1]), xmn=min(lon_all), xmx=max(lon_all), ymn=min(lat_all), ymx=max(lat_all), crs=CRS(projection(border)))
+spec_ras_speci <- raster(t(spec_mat[,76:1]), xmn=min(lon_all), xmx=max(lon_all),
+                         ymn=min(lat_all), ymx=max(lat_all), crs=CRS(projection(border)))
+sensi_ras_speci <- raster(t(sensi_mat[,76:1]), xmn=min(lon_all), xmx=max(lon_all),
+                          ymn=min(lat_all), ymx=max(lat_all), crs=CRS(projection(border)))
+miscla_ras_speci <- raster(t(miscla_mat[,76:1]), xmn=min(lon_all), xmx=max(lon_all),
+                          ymn=min(lat_all), ymx=max(lat_all), crs=CRS(projection(border)))
 cuts <- seq(0,1, by = 0.1)
-pal <- colorRampPalette(c("red", "yellow"))
 x11(width = 1000, height = 600)
 plot(spec_ras_speci,asp=1, breaks=cuts, col=plasma(length(cuts)),
      main=paste("Specificity, ", model_name, " regression\nThreshold bad yield ",
@@ -321,4 +360,61 @@ plot(sensi_ras_speci,asp=1, breaks=cuts, col=plasma(length(cuts)),
      main=paste("Sensitivity, ", model_name, " regression\nThreshold bad yield ",
                 threshold, " ; threshold segregation", segreg_th));plot(border,add=T)
 
+tryyyy <- numeric()
+for (i in 1:965) {
+  tryyyy[i]<-mis_clas_err[[i]]
+}
+max(tryyyy)
+cuts2 <- seq(0,0.2, by = 0.02)
+x11(width = 1000, height = 600)
+plot(miscla_ras_speci,asp=1, breaks=cuts2, col=viridis(length(cuts2)),
+     main=paste("Misclassification Error, ", model_name, " regression\nThreshold bad yield ",
+                threshold, " ; threshold segregation", segreg_th));plot(border,add=T)
 
+# Most important coefficients ####
+##################################
+set.seed(2019)
+
+
+firstcoeffs <- list()
+pixel_i_want <-sample(1:965, size = 1)
+
+rownames(coefs[[pixel_i_want]])[sort(abs(as.numeric(coefs[[pixel_i_want]])),
+                          decreasing = T,
+                          index.return=T)$ix[-which(sort(abs(as.numeric(coefs[[pixel_i_want]])), 
+                                                                        decreasing = T,
+                                                                        index.return=T)$ix==1)]]
+# Is it the same if I run the model on standardized data (rather than rescaled)?
+Stand_meteo_data <- apply(Model_data[pixel_i_want,-1,], FUN = scale, MARGIN = 1)
+Stand_data <- t(cbind(Model_data[pixel_i_want,1,], Stand_meteo_data))
+
+training_data_pix <- Stand_data[,training_indices[[pixel_i_want]]]
+testing_data_pix <- Stand_data[,testing_indices[[pixel_i_want]]]
+
+pix_in <- 1:pix_num
+
+x1_train_pix <- as.data.frame(t(training_data_pix[non_na_col[pixel_i_want,],])) # predictors
+y1_train_pix <- training_data_pix[1,] # predictand
+x1_test_pix <- as.data.frame(t(testing_data_pix[non_na_col[pixel_i_want,],])) # predictors
+y1_test_pix <- testing_data_pix[1,] # predictand
+
+var_num_pix <- sum(non_na_col[pixel_i_want,])
+numLevels_pix <- rep(1,times=var_num[pixel_i_want])
+numLevels_pix <-  colnames(x1_test_pix)
+
+model_1pix <- cv.glmnet(x = as.matrix(x1_train_pix),
+                        y = as.matrix(y1_train_pix),
+                        family = "binomial", alpha = no_model, nfolds = 10)
+coef_1pix <- coef(model_1pix)
+
+firstcoeffs[[5]] <- cbind(rownames(coefs[[pixel_i_want]])[sort(abs(as.numeric(coef_1pix)),
+                                                       decreasing = T,
+                                                       index.return=T)$ix[-which(sort(abs(as.numeric(coef_1pix)), 
+                                                                                      decreasing = T,
+                                                                                      index.return=T)$ix==1)]][1:10],
+                  rownames(coefs[[pixel_i_want]])[sort(abs(as.numeric(coefs[[pixel_i_want]])),
+                                                       decreasing = T,
+                                                       index.return=T)$ix[-which(sort(abs(as.numeric(coefs[[pixel_i_want]])), 
+                                                                                      decreasing = T,
+                                                                                      index.return=T)$ix==1)]][1:10])
+colnames(firstcoeffs[[5]])=c("Standardization", "Rescale with RANGE")
