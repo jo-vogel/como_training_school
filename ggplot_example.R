3bef2b8e63dd1ac58444a93dfd9d672f77eb4084
PauRiv <pauline.rivoire@giub.unibe.ch>
2019-11-15 18:51:17 +0100

    More beautiful map

--- ggplot_example.R
+++ ggplot_example.R
@@ -7,16 +7,19 @@ DF_miscla <- data.frame(lon=coord_subset[,1], lat = coord_subset[,2], miscla = m
 
 ggplot(data = DF_miscla, aes(x=lon, y=lat)) +
   geom_polygon(data = world, aes(long, lat, group=group),
-               fill="transparent", color="black", size=0.3) +
+               fill="white", color="black", size=0.3) +
   geom_point(shape=15, aes(color=miscla)) +
-  scale_color_gradient2(limits=c(0,0.2), midpoint = 0.1,  #lowest and highest value of the range to color. mid=value where to put the mid color
+  scale_color_gradient2(limits=c(0,0.2), midpoint = 0.1,
                         low = "yellow", mid = "red3", high = "black") +
-  theme_void()+
-  coord_fixed(xlim = c(min(coord_subset[,1])-1, max(coord_subset[,1]+1)),
+  theme(panel.ontop = F, panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA))+
+  coord_fixed(xlim = c(-120, 135),
               ylim = c(min(coord_subset[,2])-1, max(coord_subset[,2]+1)),
               ratio = 1.3)+
-  labs(color="Misclassification\n error",
-       title = paste(model_name,"regression"),
+  labs(color="Misclass.\nerror",
+       title = paste("Misclassification error, simple",model_name,"regression"),
        subtitle = paste("Bad yield threshold=", threshold,
                         ", segregation threshold=", segreg_th, sep = ""))+
-  X11(width = 20, height = 6)
+  theme(plot.title = element_text(size = 20), plot.subtitle = element_text(size = 15),
+        legend.title = element_text(size = 15), legend.text = element_text(size = 14)) +
+  X11(width = 20, height = 7)
