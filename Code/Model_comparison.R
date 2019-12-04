52539013e5bd87ff344c606a5bba539dc9c64972
JVogel <joschavogel@uni-potsdam.de>
2019-12-04 11:04:29 +0100

    Creation of file for model comparison

---
+++ Code/Model_comparison.R
@@ -0,0 +1,99 @@
+# Model comparison ###################
+# Authors: Cristina, Johannes, Pauline
+# This file is meant to compare the performance of all the models
+# It is structured in the following way:
+# a) load model output for each model
+# b) calculate performance metrics for each model
+# c) use these metrics to create model comparison plots
+######################################
+
+
+
+
+# Load Model output for bestglm ####
+####################################
+
+
+
+
+
+
+# Create specificity, CSI and EDI for bestglm ####
+##################################################
+
+
+
+
+
+
+# Load Model output for Ridge ####
+####################################
+
+
+
+
+
+
+# Create specificity, CSI and EDI for Ridge ####
+##################################################
+
+
+
+
+
+
+# Load Model output for Lasso w/o interactions ####
+##################################################
+
+
+
+
+
+
+# Create specificity, CSI and EDI for Lasso w/o interactions ####
+#################################################################
+
+
+
+
+
+
+# Load Model output for Lasso w/o interactions ####
+###################################################
+
+
+
+
+
+
+# Create specificity, CSI and EDI forLasso w/o interactions ####
+################################################################
+
+
+
+
+
+
+
+
+# Create comparison maps ####
+#############################
+
+
+
+# Specificity map #####
+#######################
+
+
+
+
+
+# CSI map ####
+##############
+
+
+
+
+
+# EDI map ####
+##############
