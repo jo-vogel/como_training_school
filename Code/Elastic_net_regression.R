33aab274ecb55be900de9280afd4fdb0845d1e0d
PauRiv <pauline.rivoire@giub.unibe.ch>
2019-12-13 13:23:31 +0100

    Run and plot Elastic net regression

--- Code/Elastic_net_regression.R
+++ Code/Elastic_net_regression.R
@@ -10,9 +10,6 @@ rm(list=ls(all=TRUE))
 
 model_name <- "Elastic-net"
 
-#which lambda?
-lambda_VALS <- c("lambda.min", "lambda.1se")
-lambda_val <- lambda_VALS[1]
 
 #threshold for bad yields in c(0.025,0.05,0.1)
 threshold <- 0.05
@@ -35,142 +32,62 @@ library(tictoc)
 #############################
 
 
+# recreate the training and testing dataset ####
+################################################
 
-
-# Get the data
-# path_to_NH_files <- "/scratch3/pauline/Damocles_training_school_Como2019/GroupProject1/Data/NH"
+message('Adjust the path accordingly.')
+# Laptop Pauline:
 path_to_NH_files <- "C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/Data/Global"
+output_path <- "C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/Images/Model_Comparison/"
 
-nh_files <- list.files(path=path_to_NH_files,pattern="NH_yield*") # all files from northern hemisphere
-nh_data <- lapply(1:length(nh_files),
-                  FUN = function(x){nc_open(paste0(path_to_NH_files,"/",nh_files[x]))})
-yield <- ncvar_get(nh_data[[1]],"yield")
-tasmax <- ncvar_get(nh_data[[1]],"tasmax")
-vpd <- ncvar_get(nh_data[[1]],"vpd")
-pr <- ncvar_get(nh_data[[1]],"pr")
-lat_subset <- ncvar_get(nh_data[[1]],"lat")
-lon_subset <- ncvar_get(nh_data[[1]],"lon")
-yield_stand <- ncvar_get(nh_data[[2]],"yield")
-tasmax_stand <- ncvar_get(nh_data[[2]],"tasmax")
-vpd_stand <- ncvar_get(nh_data[[2]],"vpd")
-pr_stand <- ncvar_get(nh_data[[2]],"pr")
-lapply(1:length(nh_files),function(x){nc_close(nh_data[[x]])})
-coord_subset <- cbind(lon_subset,lat_subset)
+# Johannes:
+# path_to_NH_files <- "D:/user/vogelj/Data/Group project Como"
+# output_path <- "D:/user/vogelj/Group_project/Output/Plots/"
 
-
-# load all coordinates of northern hemisphere
-nh_files <- list.files(path=path_to_NH_files,pattern="*NH.nc") # all files from northern hemisphere
-nh_data <- lapply(1:length(nh_files),function(x){nc_open(paste0(path_to_NH_files,"/",nh_files[x]))})
-lat_all <- ncvar_get(nh_data[[1]],"lat")
-lon_all <- ncvar_get(nh_data[[1]],"lon")
-lati_all <- rep(lat_all,each=length(lon_all))
-long_all <- rep(lon_all,length(lat_all)) # coordinates rearranged
-coord_all <- cbind(long_all,lati_all)
-
-lapply(1:length(nh_files),function(x){nc_close(nh_data[[x]])})
-
-
-# Process data ####
-###################
-
-yields_3dim <- array(yield,dim=c(965,1,1600));yields_stand_3dim <- array(yield,dim=c(965,1,1600))
-Model_data <- abind(yields_3dim,tasmax,vpd,pr,along=2)
-Model_data_stand <- abind(yields_stand_3dim,tasmax_stand,vpd_stand,pr_stand,along=2)
-
-
-pix_num <- dim(Model_data)[1]
-low_yield <- sapply(1:pix_num,function(x) {quantile(yield[x,],threshold,na.rm=T)})
-cy <- t(sapply(1:pix_num,function(x){ifelse(yield[x,]<low_yield[x],0,1)})) # identical for standardised and non-standardised yield
-
-cy_reshaped <- array(data=cy,dim=c(dim(cy)[1],1,1600))
-Model_data[,1,] <-cy_reshaped
-Model_data_stand[,1,] <-cy_reshaped
-
-columnnames <- c("Yield",
-                 "tmax_Aug_Y1","tmax_Sep_Y1","tmax_Oct_Y1","tmax_Nov_Y1","tmax_Dec_Y1","tmax_Jan_Y2",
-                 "tmax_Feb_Y2","tmax_Mar_Y2","tmax_Apr_Y2","tmax_May_Y2","tmax_Jun_Y2","tmax_Jul_Y2",
-                 "tmax_Aug_Y2","tmax_Sep_Y2","tmax_Oct_Y2","tmax_Nov_Y2","tmax_Dec_Y2",
-                 "vpd_Aug_Y1","vpd_Sep_Y1","vpd_Oct_Y1","vpd_Nov_Y1","vpd_Dec_Y1","vpd_Jan_Y2",
-                 "vpd_Feb_Y2","vpd_Mar_Y2","vpd_Apr_Y2","vpd_May_Y2","vpd_Jun_Y2","vpd_Jul_Y2",
-                 "vpd_Aug_Y2","vpd_Sep_Y2","vpd_Oct_Y2","vpd_Nov_Y2","vpd_Dec_Y2",
-                 "pr_Aug_Y1","pr_Sep_Y1","pr_Oct_Y1","pr_Nov_Y1","pr_Dec_Y1","pr_Jan_Y2",
-                 "pr_Feb_Y2","pr_Mar_Y2","pr_Apr_Y2","pr_May_Y2","pr_Jun_Y2","pr_Jul_Y2",
-                 "pr_Aug_Y2","pr_Sep_Y2","pr_Oct_Y2","pr_Nov_Y2","pr_Dec_Y2")
-colnames(Model_data) <- columnnames
-colnames(Model_data_stand) <- columnnames
-
-
-
-# Exclude NA variable columns
-na_col <- matrix(data=NA,nrow=pix_num,ncol=52)
-for (j in 1:pix_num){
-  for (i in 1:52){
-    na_col[j,i] <- all(is.na(Model_data[j,i,])) # TRUE if entire column is NA
-  }
-}
-non_na_col <- !na_col # columns without NAs
-non_na_col[,1] <- FALSE # exclude yield (it is no predictor and should therefore be ignored)
-
-
-na_time <- vector("list",length=pix_num) # for each pixel, the positions of NAs over time
-for (j in 1:pix_num){
-  na_time[[j]] <- which(is.na(Model_data[j,1,])) # locations of years with NA values
-}
-
-
-
-
-# Exclude years with NAs (not the whole pixel, just the NA years of it) ####
-############################################################################
-
-# Split data into training and testing data set
-vec <- 1:1600
-
-years_with_na <- vector("logical",length=pix_num)
-for (i in 1:pix_num){
-  years_with_na[i] <- ifelse(length(na_time[[i]] ) ==0,F,T)
-}
-
-training_indices <- vector("list",length=pix_num)
-testing_indices <- vector("list",length=pix_num)
-set.seed(1994)
-for (x in 1:pix_num) {
-  if (years_with_na[x]) {
-    training_indices[[x]] <- sort(sample(x=vec[-na_time[[x]]], size = floor((1600-length(na_time[[x]]))*0.6)))
-    testing_indices[[x]] <- vec[-c(na_time[[x]], training_indices[[x]])]
-  } else {
-    training_indices[[x]] <- sort(sample(1:1600, size = floor(1600*0.6)))
-    testing_indices[[x]] <- (1:1600)[-training_indices[[x]]]    
-  }
-}
-# training_indices2 <- sapply(1:pix_num, function(x) {sort(sample(x=vec[-na_time[[x]]], size = floor((1600-length(na_time[[x]]))*0.6)))})
-# testing_indices2 <- sapply(1:pix_num, function(x) {vec[-na_time[[x]]][-training_indices[[x]]]})
-# training_indices2 <- sapply(1:pix_num, function(x) {sort(sample(1:(1600-length(na_time[[x]])), size = floor((1600-length(na_time[[x]]))*0.6)))})
-# testing_indices2 <- sapply(1:pix_num, function(x) {(1:(1600-length(na_time[[x]])))[-training_indices2[[x]]]})
-
-Training_Data <- lapply(1:pix_num,function(x){Model_data_stand[x,,training_indices[[x]]]})
-Testing_Data <- lapply(1:pix_num,function(x){Model_data_stand[x,,testing_indices[[x]]]})
-
-pix_in <- 1:pix_num
-# x1_train_list <- lapply(seq_along(pix_in), function(x){ as.data.frame(t(Training_Data[x,non_na_col[x,],]))}) # predictors
-# y1_train_list <- lapply(seq_along(pix_in), function(x){ Training_Data[x,1,]}) # predictand
-# x1_test_list <- lapply(seq_along(pix_in), function(x){ as.data.frame(t(Testing_Data[x,non_na_col[x,],]))}) # predictors
-# y1_test_list <- lapply(seq_along(pix_in), function(x){Testing_Data[x,1,]}) # predictand
-
-x1_train_list <- lapply(seq_along(pix_in), function(x){ as.data.frame(t(Training_Data[[x]][non_na_col[x,],]))}) # predictors
-y1_train_list <- lapply(seq_along(pix_in), function(x){ Training_Data[[x]][1,]}) # predictand
-x1_test_list <- lapply(seq_along(pix_in), function(x){ as.data.frame(t(Testing_Data[[x]][non_na_col[x,],]))}) # predictors
-y1_test_list <- lapply(seq_along(pix_in), function(x){Testing_Data[[x]][1,]}) # predictand
-
-var_num <- apply(non_na_col,1,sum)
-numLevels_list <- sapply(1:pix_num, function(x){ rep(1,times=var_num[x])})
-for (i in 1:pix_num){
-  names(numLevels_list[[i]]) <-  colnames(x1_test_list[[i]])
-}
+source('./Code/Lasso_interact_global_preparation.R') # load necessary files
 
 
 
 ##### Run the model ####
+
+#Value for alpha, in (0;1)
+alpha_elastic <- 0.5
+set.seed(2019)
+
+crossval <- list()
+
+fit_pix_lambdamin <- list()
+fit_pix_lambda1se <- list()
+
+for (pixel in 1:dim(Model_data_stand)[1]) {
+  print(paste("Pixel",pixel,"out of",dim(Model_data_stand)[1]))
+  tic()
+  crossval[[pixel]] <- cv.glmnet(x = as.matrix(x1_train_list[[pixel]]),
+                                 y = as.matrix(y1_train_list[[pixel]]),
+                                 family = "binomial", alpha = alpha_elastic, nfolds = 10)
+  fit_pix_lambdamin[[pixel]] <- glmnet(x = as.matrix(x1_train_list[[pixel]]),
+                                       y = as.matrix(y1_train_list[[pixel]]),
+                                       family = "binomial", alpha = alpha_elastic,
+                                       lambda = crossval[[pixel]]$lambda.min)
+  fit_pix_lambda1se[[pixel]] <- glmnet(x = as.matrix(x1_train_list[[pixel]]),
+                                       y = as.matrix(y1_train_list[[pixel]]),
+                                       family = "binomial", alpha = alpha_elastic,
+                                       lambda = crossval[[pixel]]$lambda.1se)
+  toc()
+}#end for pixel
+
+
+save(crossval, file = paste("C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/cv",
+                            model_name,"_alpha",str_pad(alpha_elastic*10, 2, pad = "0"),"_threshbadyield",
+                            str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+save(fit_pix_lambdamin, file = paste("C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/",
+                                     model_name,"_lambdamin_alpha",str_pad(alpha_elastic*10, 2, pad = "0"),"_threshbadyield",
+                                     str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+
+save(fit_pix_lambda1se, file = paste("C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/",
+                                    model_name,"_lambda1se_alpha",str_pad(alpha_elastic*10, 2, pad = "0"),"_threshbadyield",
+                                     str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+
 # Cross validation to find the good alpha
 
 
@@ -266,3 +183,163 @@ toc()
 #                  y = as.matrix(y1_train_list[[pixel]]),
 #                  family = "binomial", lambda = min_cvm["lambda.1se"], alpha = min_cvm["alpha"])
 # coef(result)
+
+
+
+
+##### Load the model ####
+alpha_elastic <- 0.5
+
+#which lambda?
+lambda_VALS <- c("lambda.min", "lambda.1se")
+lambda_val <- lambda_VALS[2]
+
+if(lambda_val == "lambda.min"){
+  load(file = paste("C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/",
+                  model_name,"_lambdamin_alpha",str_pad(alpha_elastic*10, 2, pad = "0"),"_threshbadyield",
+                  str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+  model_fit <- fit_pix_lambdamin
+  rm(fit_pix_lambdamin)
+}
+
+if(lambda_val == "lambda.1se"){
+  load(file = paste("C:/Users/admin/Documents/Damocles_training_school_Como/GroupProject1/RidgeRegression/Global_results/",
+                  model_name,"_lambda1se_alpha",str_pad(alpha_elastic*10, 2, pad = "0"),"_threshbadyield",
+                  str_pad(threshold*100, 3, pad = "0"),".RData", sep = ""))
+  model_fit <- fit_pix_lambda1se
+  rm(fit_pix_lambda1se)
+}
+
+
+
+# Model performance ###
+#######################
+#Start with Lambda min
+
+test_length <- length(model_fit)
+
+coefs <- lapply(1:test_length, function(x){coef(model_fit[[x]])})
+
+
+
+mypred <- lapply(1:test_length, function(x){predict(model_fit[[x]],
+                                                    as.matrix(x1_test_list[[x]]),type="response")})
+
+#which segregation threshold for the model?
+segreg_th <- 0.5
+
+fitted.results_model <- lapply(1:test_length, function(x){ifelse(mypred[[x]] > segreg_th,1,0)})
+
+mis_clas_err <- sapply(1:test_length, function(x){misClassError(actuals = as.matrix(y1_test_list[[x]]),
+                                                                predictedScores=mypred[[x]],
+                                                                threshold = segreg_th)})
+
+con_tab <-  lapply(1:test_length, function(x){InformationValue::confusionMatrix(as.matrix(y1_test_list[[x]]),
+                                                                                fitted.results_model[[x]],
+                                                                                threshold = segreg_th)})
+
+speci <- sapply(1:test_length, function(x){InformationValue::specificity(as.matrix(y1_test_list[[x]]),
+                                                                         fitted.results_model[[x]],
+                                                                         threshold = segreg_th)})
+
+# CSI #
+csi <- numeric()
+for(pix in 1:length(coefs)){
+  csi[pix] <- con_tab[[pix]]["0","0"]/(con_tab[[pix]]["0","0"] + con_tab[[pix]]["1","0"] + con_tab[[pix]]["0","1"])
+  if(is.na(con_tab[[pix]]["0","0"])){
+    csi[pix] <- 0
+  }
+}#end for pix
+
+# Plots ####
+world <- map_data("world")
+
+# Plot specificity error ####
+
+DF_speci <- data.frame(lon=coord_subset[,1], lat = coord_subset[,2], speci = speci)
+
+ggplot(data = DF_speci, aes(x=lon, y=lat)) +
+  geom_polygon(data = world, aes(long, lat, group=group),
+               fill="white", color="black", size=0.3) +
+  geom_point(shape=15, aes(color=speci)) +
+  scale_color_gradient2(limits=c(0,1), midpoint = 0.5,
+                        low = "black", mid = "red3", high = "yellow") +
+  theme(panel.ontop = F, panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA),
+        axis.text = element_text(size = 15), axis.title = element_text(size = 15))+
+  ylab("Lat (°N)") +
+  xlab("Lon (°E)") +
+  coord_fixed(xlim = c(-120, 135),
+              ylim = c(min(coord_subset[,2])-1, max(coord_subset[,2]+1)),
+              ratio = 1.3)+
+  labs(color="Specif.",
+       title = paste("Monthly data: specificity, simple",model_name,"regression"),
+       subtitle = paste("Bad yield threshold=", threshold,
+                        ", segregation threshold=", segreg_th, sep = ""))+
+  theme(plot.title = element_text(size = 20), plot.subtitle = element_text(size = 15),
+        legend.title = element_text(size = 15), legend.text = element_text(size = 14)) +
+  X11(width = 20, height = 7)
+
+
+# plot CSI=(hits)/(hits + misses + false alarm) ###
+DF_csi <- data.frame(lon=coord_subset[,1], lat = coord_subset[,2], csi = csi)
+
+ggplot(data = DF_csi, aes(x=lon, y=lat)) +
+  geom_polygon(data = world, aes(long, lat, group=group),
+               fill="white", color="black", size=0.3) +
+  geom_point(shape=15, aes(color=csi)) +
+  scale_color_gradient2(limits=c(0,1), midpoint = 0.5,
+                        low = "black", mid = "red3", high = "yellow") +
+  theme(panel.ontop = F, panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA),
+        axis.text = element_text(size = 15), axis.title = element_text(size = 15))+
+  ylab("Lat (°N)") +
+  xlab("Lon (°E)") +
+  coord_fixed(xlim = c(-120, 135),
+              ylim = c(min(coord_subset[,2])-1, max(coord_subset[,2]+1)),
+              ratio = 1.3)+
+  labs(color="csi",
+       title = paste("Seasonal data: critical success index, simple",model_name,"regression"),
+       subtitle = paste("Bad yield threshold=", threshold,
+                        ", segregation threshold=", segreg_th, sep = ""))+
+  theme(plot.title = element_text(size = 20), plot.subtitle = element_text(size = 15),
+        legend.title = element_text(size = 15), legend.text = element_text(size = 14)) +
+  X11(width = 20, height = 7)
+
+
+
+# Plot number of variables kept ###
+coeff_kep <- numeric()
+
+for (pix in 1:965) {
+  # coeff_kep[pix] <- length(coefs[[pix]]$mainEffects$cont)
+  coeff_kep[pix] <- sum(coefs[[pix]][row.names(coefs[[pix]])!="(Intercept)"]!=0)
+  
+}#end for pix
+
+
+
+# Plot 
+
+DF_numbcoeff <- data.frame(lon=coord_subset[,1], lat = coord_subset[,2], coeff_kep = coeff_kep)
+
+ggplot(data = DF_numbcoeff, aes(x=lon, y=lat)) +
+  geom_polygon(data = world, aes(long, lat, group=group),
+               fill="white", color="black", size=0.3) +
+  geom_point(shape=15, aes(color=coeff_kep)) +
+  scale_color_gradient(limits=c(1,39),
+                       low = "pink", high = "darkblue") +
+  theme(panel.ontop = F, panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA),
+        axis.text = element_text(size = 15), axis.title = element_text(size = 15))+
+  ylab("Lat (°N)") +
+  xlab("Lon (°E)") +
+  coord_fixed(xlim = c(-120, 135),
+              ylim = c(min(coord_subset[,2])-1, max(coord_subset[,2]+1)),
+              ratio = 1.3)+
+  labs(color="Nb of var.",
+       title = paste("Number of variables kept, simple",model_name,"regression"),
+       subtitle = lambda_val)+
+  theme(plot.title = element_text(size = 20), plot.subtitle = element_text(size = 15),
+        legend.title = element_text(size = 15), legend.text = element_text(size = 14)) +
+  X11(width = 20, height = 7)
