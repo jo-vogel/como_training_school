b1ce71f0a6c4ff418265a816d7bbad8b4cfd4771
PauRiv <pauline.rivoire@giub.unibe.ch>
2020-06-12 11:26:21 +0200

    Complete comments and improve readability

--- Code/Lasso_regression.R
+++ Code/Lasso_regression.R
@@ -4,8 +4,8 @@
 # Structure of the code
 # a) Load the standardized data (meteorological variables and extremal indicators)
 # b) Create training and testing dataset
-# c) Run the cross validation
-# d) 
+# c) Run the cross validation to find lambda.min and lambda.1se
+# d) Run the Lasso regression 
 
 
 # Required libraries ####
@@ -22,39 +22,41 @@ pix_num <- length(Data_xtrm_standardized$longitudes)
 nb_years <- dim(Data_xtrm_standardized$yield)[2]
 
 # Create arrays of data #####
-yield_3dim <- array(Data_xtrm_standardized$yield,dim=c(pix_num,1,nb_years))
-dtr_3dim <- array(Data_xtrm_standardized$dtr,dim=c(pix_num,1,nb_years))
-frs_3dim <- array(Data_xtrm_standardized$frs,dim=c(pix_num,1,nb_years))
-txx_3dim <- array(Data_xtrm_standardized$txx,dim=c(pix_num,1,nb_years))
-tnn_3dim <- array(Data_xtrm_standardized$tnn,dim=c(pix_num,1,nb_years))
-rx5_3dim <- array(Data_xtrm_standardized$rx5,dim=c(pix_num,1,nb_years))
-tx90p_3dim <- array(Data_xtrm_standardized$tx90p,dim=c(pix_num,1,nb_years))
-tn10p_3dim <- array(Data_xtrm_standardized$tn10p,dim=c(pix_num,1,nb_years))
+yield_3dim <- array(Data_xtrm_standardized$yield, dim = c(pix_num,1,nb_years))
+dtr_3dim <- array(Data_xtrm_standardized$dtr, dim = c(pix_num,1,nb_years))
+frs_3dim <- array(Data_xtrm_standardized$frs, dim = c(pix_num,1,nb_years))
+txx_3dim <- array(Data_xtrm_standardized$txx, dim = c(pix_num,1,nb_years))
+tnn_3dim <- array(Data_xtrm_standardized$tnn, dim = c(pix_num,1,nb_years))
+rx5_3dim <- array(Data_xtrm_standardized$rx5, dim = c(pix_num,1,nb_years))
+tx90p_3dim <- array(Data_xtrm_standardized$tx90p, dim = c(pix_num,1,nb_years))
+tn10p_3dim <- array(Data_xtrm_standardized$tn10p, dim = c(pix_num,1,nb_years))
 
-Model_data <- abind(yield_3dim,dtr_3dim,frs_3dim,txx_3dim,tnn_3dim,rx5_3dim,tx90p_3dim,tn10p_3dim
-                    ,Data_xtrm_standardized$tasmax,Data_xtrm_standardized$vpd,Data_xtrm_standardized$pr,along=2)
+Model_data <- abind(yield_3dim, dtr_3dim, frs_3dim, txx_3dim, tnn_3dim, rx5_3dim, tx90p_3dim, tn10p_3dim
+                    , Data_xtrm_standardized$tasmax, Data_xtrm_standardized$vpd, Data_xtrm_standardized$pr, along = 2)
 colnames(Model_data) <- c("Yield", "dtr", "frs", "txx", "tnn", "rx5", "tx90p", "tn10p",
-                          "tmax_Aug_Y1","tmax_Sep_Y1","tmax_Oct_Y1","tmax_Nov_Y1","tmax_Dec_Y1","tmax_Jan_Y2",
-                          "tmax_Feb_Y2","tmax_Mar_Y2","tmax_Apr_Y2","tmax_May_Y2","tmax_Jun_Y2","tmax_Jul_Y2",
-                          "tmax_Aug_Y2","tmax_Sep_Y2","tmax_Oct_Y2","tmax_Nov_Y2","tmax_Dec_Y2",
-                          "vpd_Aug_Y1","vpd_Sep_Y1","vpd_Oct_Y1","vpd_Nov_Y1","vpd_Dec_Y1","vpd_Jan_Y2",
-                          "vpd_Feb_Y2","vpd_Mar_Y2","vpd_Apr_Y2","vpd_May_Y2","vpd_Jun_Y2","vpd_Jul_Y2",
-                          "vpd_Aug_Y2","vpd_Sep_Y2","vpd_Oct_Y2","vpd_Nov_Y2","vpd_Dec_Y2",
-                          "pr_Aug_Y1","pr_Sep_Y1","pr_Oct_Y1","pr_Nov_Y1","pr_Dec_Y1","pr_Jan_Y2",
-                          "pr_Feb_Y2","pr_Mar_Y2","pr_Apr_Y2","pr_May_Y2","pr_Jun_Y2","pr_Jul_Y2",
-                          "pr_Aug_Y2","pr_Sep_Y2","pr_Oct_Y2","pr_Nov_Y2","pr_Dec_Y2")
+                          "tmax_Aug_Y1", "tmax_Sep_Y1", "tmax_Oct_Y1", "tmax_Nov_Y1", "tmax_Dec_Y1", "tmax_Jan_Y2",
+                          "tmax_Feb_Y2", "tmax_Mar_Y2", "tmax_Apr_Y2", "tmax_May_Y2", "tmax_Jun_Y2", "tmax_Jul_Y2",
+                          "tmax_Aug_Y2", "tmax_Sep_Y2", "tmax_Oct_Y2", "tmax_Nov_Y2", "tmax_Dec_Y2",
+                          "vpd_Aug_Y1", "vpd_Sep_Y1", "vpd_Oct_Y1", "vpd_Nov_Y1", "vpd_Dec_Y1", "vpd_Jan_Y2",
+                          "vpd_Feb_Y2", "vpd_Mar_Y2", "vpd_Apr_Y2", "vpd_May_Y2", "vpd_Jun_Y2", "vpd_Jul_Y2",
+                          "vpd_Aug_Y2", "vpd_Sep_Y2", "vpd_Oct_Y2", "vpd_Nov_Y2", "vpd_Dec_Y2",
+                          "pr_Aug_Y1", "pr_Sep_Y1", "pr_Oct_Y1", "pr_Nov_Y1", "pr_Dec_Y1", "pr_Jan_Y2",
+                          "pr_Feb_Y2", "pr_Mar_Y2", "pr_Apr_Y2", "pr_May_Y2", "pr_Jun_Y2", "pr_Jul_Y2",
+                          "pr_Aug_Y2", "pr_Sep_Y2", "pr_Oct_Y2", "pr_Nov_Y2", "pr_Dec_Y2")
 
 Yield <- Data_xtrm_standardized$yield
-low_yield <- apply(Yield, MARGIN = 1, FUN=quantile, probs=threshold, na.rm=T)
-cy <- t(sapply(1:pix_num,function(x){ifelse(Yield[x,]<low_yield[x],0,1)})) # identical for standardised and non-standardised yield
 
-cy_reshaped <- array(data=cy,dim=c(dim(cy)[1],1,nb_years))
+threshold <- 0.05 #threshold for bad yields
+low_yield <- apply(Yield, MARGIN = 1, FUN = quantile, probs = threshold, na.rm = T)
+cy <- t(sapply(1:pix_num, function(x){ifelse(Yield[x,]<low_yield[x],0,1)})) # identical for standardised and non-standardised yield
+
+cy_reshaped <- array(data = cy,dim = c(dim(cy)[1],1,nb_years))
 
 Model_data[,1,] <-cy_reshaped
 
 
 # Exclude NA variable columns
-na_col <- matrix(data=NA,nrow=pix_num,ncol=dim(Model_data)[2])
+na_col <- matrix(data = NA, nrow = pix_num, ncol = dim(Model_data)[2])
 for (j in 1:pix_num){
   for (i in 1:dim(Model_data)[2]){
     na_col[j,i] <- all(is.na(Model_data[j,i,])) # TRUE if entire column is NA
@@ -63,33 +65,31 @@ for (j in 1:pix_num){
 non_na_col <- !na_col # columns without NAs
 non_na_col[,1] <- FALSE # exclude yield (it is no predictor and should therefore be ignored)
 
-na_time <- vector("list",length=pix_num) # for each pixel, the positions of NAs over time
+na_time <- vector("list", length = pix_num) # for each pixel, the positions of NAs over time
 for (j in 1:pix_num){
   na_time[[j]] <- which(is.na(Model_data[j,1,])) # locations of years with NA values
 }
 
 
 # Split data into training and testing data set #####
-vec <- 1:nb_years
-
-years_with_na <- vector("logical",length=pix_num)
+years_with_na <- vector("logical", length = pix_num)
 for (i in 1:pix_num){
-  years_with_na[i] <- ifelse(length(na_time[[i]] ) ==0,F,T)
+  years_with_na[i] <- ifelse(length(na_time[[i]] ) == 0, F, T)
 }
 
-training_indices <- vector("list",length=pix_num)
-testing_indices <- vector("list",length=pix_num)
+training_indices <- vector("list", length = pix_num)
+testing_indices <- vector("list", length = pix_num)
 
 
 # Parameters of the splitting
-seed=1994
+seed <- 1994
 train_size <- 70
 
 set.seed(seed)
 for (x in 1:pix_num) {
   if (years_with_na[x]) {
-    training_indices[[x]] <- sort(sample(x=vec[-na_time[[x]]], size = floor((nb_years-length(na_time[[x]]))*(train_size/100))))
-    testing_indices[[x]] <- vec[-c(na_time[[x]], training_indices[[x]])]
+    training_indices[[x]] <- sort(sample(x = (1:nb_years)[-na_time[[x]]], size = floor((nb_years-length(na_time[[x]]))*(train_size/100))))
+    testing_indices[[x]] <- (1:nb_years)[-c(na_time[[x]], training_indices[[x]])]
   } else {
     training_indices[[x]] <- sort(sample(1:nb_years, size = floor(nb_years*(train_size/100))))
     testing_indices[[x]] <- (1:nb_years)[-training_indices[[x]]]    
@@ -97,8 +97,8 @@ for (x in 1:pix_num) {
 }
 
 
-Training_Data <- lapply(1:pix_num,function(x){Model_data[x,,training_indices[[x]]]})
-Testing_Data <- lapply(1:pix_num,function(x){Model_data[x,,testing_indices[[x]]]})
+Training_Data <- lapply(1:pix_num, function(x){Model_data[x,,training_indices[[x]]]})
+Testing_Data <- lapply(1:pix_num, function(x){Model_data[x,,testing_indices[[x]]]})
 
 pix_in <- 1:pix_num
 
@@ -109,8 +109,8 @@ x1_test_list <- lapply(seq_along(pix_in), function(x){ as.data.frame(t(Testing_D
 y1_test_list <- lapply(seq_along(pix_in), function(x){Testing_Data[[x]][1,]}) # predictand
 
 
-var_num <- apply(non_na_col,1,sum)
-numLevels_list <- sapply(1:pix_num, function(x){ rep(1,times=var_num[x])})
+var_num <- apply(non_na_col, 1, sum)
+numLevels_list <- sapply(1:pix_num, function(x){ rep(1, times = var_num[x])})
 for (i in 1:pix_num){
   names(numLevels_list[[i]]) <-  colnames(x1_test_list[[i]])
 }
@@ -151,8 +151,8 @@ for (pixel in 1:pix_num) {
                                                y = yield_pix,
                                                family = "binomial",
                                                alpha = 1,
-                                               nfolds = 10) #number of folds for the cross validation
-      }#end if else too half years == 0 kg/ha
+                                               nfolds = 10)
+      }#end if else not enough bad occurences
       
     }#end if exists na else
     
@@ -169,7 +169,7 @@ save(model_cv_fitting, file = paste0(path_model, "cv_month_xtrm_LASSO_threshbady
 # Run the model with lambda1se and lambda min just obtained #####
 
 load(file = paste0(path_model, "cv_month_xtrm_LASSO_threshbadyield005_seed",
-                   seed, "_train", train_size,"_995pixels.RData"))
+                   seed, "_train", train_size, "_995pixels.RData"))
 
 
 if (model_name == "Lasso"){
@@ -212,9 +212,9 @@ if (model_name == "Lasso"){
                                                  family = "binomial",
                                                  alpha = 1,
                                                  lambda = model_cv_fitting[[pixel]]$lambda.1se)
-      }#end if exists na else
+      }#end if else there exists na in extremes
       
-    } #end ifelse
+    } #end ifelse the cross validation could not be run
     print(paste(pixel, "out of", pix_num))
     
   }#end for pixel
